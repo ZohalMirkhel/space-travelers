d6c2d1465ae73eb72f63910ccf0f6874afd973a7
ZohalMirkhel <zohalmirkhel@gmail.com>
2024-09-03 13:05:08 +0430

    Worked on the reducer of missions page.

---
+++ src/redux/reducers.js
@@ -0,0 +1,32 @@
+import { FETCH_MISSIONS, JOIN_MISSION, LEAVE_MISSION } from './action';
+
+const initialState = [];
+
+const missionsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_MISSIONS:
+      return action.payload.map((mission) => ({
+        ...mission,
+        reserved: false,
+      }));
+
+    case JOIN_MISSION:
+      return state.map((mission) =>
+        mission.id === action.payload
+          ? { ...mission, reserved: true }
+          : mission
+      );
+
+    case LEAVE_MISSION:
+      return state.map((mission) =>
+        mission.id === action.payload
+          ? { ...mission, reserved: false }
+          : mission
+      );
+
+    default:
+      return state;
+  }
+};
+
+export default missionsReducer;
