b6a25a9c44c904b1613830d351f5d72d98bd7326
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-03 09:26:33 +0430

    give navabr details

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
-// App.js
-import React from 'react';
-import Navbar from 'navbar'; 
-
-const App = () => {
-  return (
-    <div className="App">
-      <Navbar />
-      {/* Other components */}
-    </div>
-=======
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import Profile from './pages/Profile';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/rockets" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
@@ -26,8 +16,7 @@ function App() {
         <Route path="/" element={<Rockets />} />
       </Routes>
     </Router>
->>>>>>> 06485761e263a783b91de606377730e0d9368955
   );
-};
+}
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Rocket</li>
+        <li>Missions</li>
+        <li>Profile</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
