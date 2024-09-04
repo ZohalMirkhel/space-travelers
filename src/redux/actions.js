411bfeef986935866e14090e92a90ab663a9161f
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-04 23:25:59 +0430

    add action that link th profile

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,29 +1,13 @@
-import axios from 'axios';
-
-// Action Types
-export const FETCH_ROCKETS_REQUEST = 'FETCH_ROCKETS_REQUEST';
-export const FETCH_ROCKETS_SUCCESS = 'FETCH_ROCKETS_SUCCESS';
-export const FETCH_ROCKETS_FAILURE = 'FETCH_ROCKETS_FAILURE';
+// actions.js
 export const RESERVE_ROCKET = 'RESERVE_ROCKET';
-export const CANCEL_ROCKET = 'CANCEL_ROCKET';
+export const CANCEL_RESERVATION = 'CANCEL_RESERVATION';
 
-// Action Creators
-export const fetchRocketsRequest = () => ({ type: FETCH_ROCKETS_REQUEST });
-export const fetchRocketsSuccess = (rockets) => ({ type: FETCH_ROCKETS_SUCCESS, payload: rockets });
-export const fetchRocketsFailure = (error) => ({ type: FETCH_ROCKETS_FAILURE, payload: error });
+export const reserveRocket = (id) => ({
+  type: RESERVE_ROCKET,
+  payload: id,
+});
 
-export const reserveRocket = (id) => ({ type: RESERVE_ROCKET, payload: id });
-export const cancelRocket = (id) => ({ type: CANCEL_ROCKET, payload: id });
-
-// Async Action Creator for Fetching Rockets
-export const fetchRockets = () => {
-  return async (dispatch) => {
-    dispatch(fetchRocketsRequest());
-    try {
-      const response = await axios.get('https://api.spacexdata.com/v4/rockets');
-      dispatch(fetchRocketsSuccess(response.data));
-    } catch (error) {
-      dispatch(fetchRocketsFailure('Failed to fetch rockets data.'));
-    }
-  };
-};
+export const cancelReservation = (id) => ({
+  type: CANCEL_RESERVATION,
+  payload: id,
+});
