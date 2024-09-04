1053156cdb0afe39a1ef63df63b3cb3718a814cb
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-04 22:49:05 +0430

    add content section

--- src/components/RocketItem.js
+++ src/components/RocketItem.js
@@ -12,5 +12,16 @@ const RocketItem = ({ rocket, onReserve, onCancel }) => {
         />
       </div>
 
+      {/* Content Section */}
+      <div className="md:w-2/3 p-6 flex flex-col justify-between">
+        <div>
+          <h3 className="text-2xl font-bold text-gray-800 mb-3">
+            {rocket.name}
+          </h3>
+          <p className="text-gray-600 mb-4">
+            {rocket.description}
+          </p>
+        </div>
+};
 
 export default RocketItem;
