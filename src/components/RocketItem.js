277b51b8b4b5d5d8484267af06179560b20599cd
ZohalMirkhel <zohalmirkhel@gmail.com>
2024-09-06 13:56:59 +0430

    Done with the funtionality of the website once again

--- src/components/RocketItem.js
+++ src/components/RocketItem.js
@@ -1,10 +1,19 @@
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { reserveRocket, cancelReservation } from '../redux/actions';
+import { reserveRocket, cancelReservation, setReservedRockets } from '../redux/actions';
 
 const RocketItem = ({ rocket }) => {
   const dispatch = useDispatch();
   const reservedRockets = useSelector(state => state.reservedRockets.map(r => r.id));
   const isReserved = reservedRockets.includes(rocket.id);
+
+  // Sync reserved rockets from localStorage when the component mounts
+  useEffect(() => {
+    const reservedFromLocalStorage = JSON.parse(localStorage.getItem('reservedRockets')) || [];
+    if (Array.isArray(reservedFromLocalStorage)) {
+      dispatch(setReservedRockets(reservedFromLocalStorage));
+    }
+  }, [dispatch]);
 
   const handleReserve = () => {
     if (isReserved) {
@@ -18,7 +27,11 @@ const RocketItem = ({ rocket }) => {
     <div className="p-6 border border-gray-300 rounded-lg shadow-lg">
       <img src={rocket.flickr_images[0]} alt={rocket.name} className="w-full h-48 object-cover rounded-t-lg mb-4" />
       <h2 className="text-xl font-bold mb-2">{rocket.name}</h2>
-      <p className="text-gray-700 mb-4">{rocket.description}</p>
+      {/* Add the "Reserved" badge if the rocket is reserved */}
+      <p className="text-gray-700 mb-4">
+        {isReserved && <span className="text-white bg-green-500 px-2 py-1 rounded-md mr-2">Reserved</span>}
+        {rocket.description}
+      </p>
       <button
         onClick={handleReserve}
         className={`py-2 px-4 rounded-lg text-white ${isReserved ? 'bg-red-500 hover:bg-red-700' : 'bg-blue-500 hover:bg-blue-700'}`}
