c264c5feddbc4570dca7de3ea7583527b1bf61cf
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-06 12:15:29 +0430

    worked on the design of site

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import {thunk} from 'redux-thunk';
-import rocketReducer from './reducers';
+import { configureStore } from '@reduxjs/toolkit';
+import Reducer from './reducers';
 
-const composeEnhancers =
-  process.env.NODE_ENV === 'development'
-    ? window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_
-    : null || compose;
-
-const store = createStore(
-  rocketReducer,
-  composeEnhancers ? composeEnhancers(applyMiddleware(thunk)) : applyMiddleware(thunk)
-);
+const store = configureStore({
+  reducer: {
+    missions: Reducer,
+  },
+});
 
 export default store;
